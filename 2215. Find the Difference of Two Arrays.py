8134c07f021fa0f856886c6d9544e5afe0b9ef9e
athulprabha10101 <alertsforathul@gmail.com>
2023-05-08 10:00:47 +0530

    2215. Find the Difference of Two Arrays

---
+++ 2215. Find the Difference of Two Arrays.py
@@ -0,0 +1,9 @@
+def findDifference(nums1, nums2):
+    answer = [[], []]
+    for i in nums1:
+        if i not in nums2 and i not in answer[0]:
+            answer[0].append(i)
+    for j in nums2:
+        if j not in nums1 and j not in answer[1]:
+            answer[1].append(j)
+    return answer
